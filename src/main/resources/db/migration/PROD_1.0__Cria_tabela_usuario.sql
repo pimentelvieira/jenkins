e4cad9b92ac3d6ff079a4869ac04d8e00615402b
William Pimentel <pimentelvieira@yahoo.com.br>
2018-04-06 22:09:02 -0300

    Plugin Flyway

--- src/main/resources/db/migration/PROD_1.0__Cria_tabela_usuario.sql
+++ src/main/resources/db/migration/PROD_1.0__Cria_tabela_usuario.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS `jenkinsprod`.`usuario`;
+DROP TABLE IF EXISTS `jenkinsprod`.`flyway_schema_history`;
+
 CREATE TABLE `jenkinsprod`.`usuario` (
   `id` INT NOT NULL,
   `nome` VARCHAR(45) NULL,
--- src/main/resources/db/migration/UAT_1.0__Cria_tabela_usuario.sql
+++ src/main/resources/db/migration/UAT_1.0__Cria_tabela_usuario.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS `jenkinsuat`.`usuario`;
+DROP TABLE IF EXISTS `jenkinsuat`.`flyway_schema_history`;
+
 CREATE TABLE `jenkinsuat`.`usuario` (
   `id` INT NOT NULL,
   `nome` VARCHAR(45) NULL,
